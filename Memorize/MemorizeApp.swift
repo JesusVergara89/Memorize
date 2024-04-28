56be80a03ff938c1b48c36684cc31265051b014a
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-04-28 13:29:45 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jesus Manuel Vergara on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
