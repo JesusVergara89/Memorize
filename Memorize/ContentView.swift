37ad6dd404ad2c34467534bddd61c69e5712da94
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-04-28 19:19:10 -0500

    Using ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -23,19 +23,28 @@ struct ContentView: View {
 
 struct CardView: View {
     var NewText: String = "😂"
-    var isFaceUp: Bool = false
+    //var NewText = "😂" wir brauchen nicht to specify the type of varible cause es gibt something call type inference
+    @State var isFaceUp: Bool = false
+    // var isFaceUp = false so wir brachen nicht to specify the type of varibel
     var body: some View {
         ZStack {
+            let Base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            //let Base = RoundedRectangle(cornerRadius: 12) type inference, so we can put the type aber the type is implicit
+            //let Base = Circle() so we can change the struct or shape of the view ohne bracht to change allein vier places
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
+                Base.fill(.white)
+                Base
                     .strokeBorder(style: StrokeStyle(lineWidth: 2))
                 Text(NewText).font(.largeTitle)
             }else{
-                RoundedRectangle(cornerRadius: 12)
+                Base.fill()
             }
             
+        }
+        .onTapGesture {
+            //print("tapped")// dises print in the console "tapped"
+            //isFaceUp = !isFaceUp // isFaceUp ist eine strcuture, so wir konnen call function avaiables since the creation of the varible itselft.
+            isFaceUp.toggle() // toggle() change the bool varibale between true and false
         }
     }
 }
